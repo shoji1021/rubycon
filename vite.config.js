8e56cbdcee2c3dee17cdb324f5fe31f90af92d96
shoji1021 <shoji.masato@it-mirai-h.ibk.ed.jp>
2025-09-29 21:15:43 +0900

    cc

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  base: './',
   build: {
     rollupOptions: {
       input: {
